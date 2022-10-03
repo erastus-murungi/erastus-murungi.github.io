d15c19c554f0207c5fba4a5cff45c6d7d48651bb
Erastus Murungi <erastuswagwan@gmail.com>
2022-10-03 17:56:21 -0400

    added a few more components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { MiscComponent } from './misc/misc.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'misc',
+    component: MiscComponent
+  },
+  {
+    path: 'home',
+    component: AboutComponent
+  },
+  { path: '**',   
+  component: PageNotFoundComponent 
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
