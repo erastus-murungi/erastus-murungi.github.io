b37eb4259efc1550222e3d459916b23f722d668b
Erastus Murungi <erastuswagwan@gmail.com>
2024-10-22 22:47:33 -0700

    revert asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   distDir: "dist",
-  assetPrefix: isProd ? "/erastus-murungi.github.io/" : "",
   basePath: isProd ? "/erastus-murungi.github.io" : "",
   images: {
     unoptimized: true,
