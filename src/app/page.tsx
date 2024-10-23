4c476e447baf84a0b7d59b6e4eb8fd7fd458b1f9
Erastus Murungi <erastuswagwan@gmail.com>
2024-10-22 22:52:17 -0700

    we're barely beginning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import { getImageUrl } from "../lib/image-utils";
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-center p-8">
+        Look at my Pepi Pepi ❤️ 👉🏾
+      </h1>
       <Image
         src={getImageUrl("pic.jpg")}
         alt="Pepi"
