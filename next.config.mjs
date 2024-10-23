ae7d551026c6eafccfe9812bff502002968d5ddc
Erastus Murungi <erastuswagwan@gmail.com>
2024-10-22 20:21:47 -0700

    another attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   distDir: "dist",
+  assetPrefix: isProd ? "/erastus-murungi.github.io/" : "",
+  basePath: isProd ? "/erastus-murungi.github.io" : "",
   images: {
     unoptimized: true,
   },
