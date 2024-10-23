c078b63bc3ffb800a4410af722975a8705cdbb7d
Erastus Murungi <erastuswagwan@gmail.com>
2024-10-22 17:49:30 -0700

    add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <h1>Work in progress</h1>
+      <Image
+        src="/IMG_0018.jpg"
+        alt="Pepi"
+        width={500}
+        height={500}
+        className="border-black border-4 p-8"
+      />
     </div>
   );
 }
