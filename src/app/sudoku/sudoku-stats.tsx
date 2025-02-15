f4d7e6a8842c5ffeb51bfdf3a30faf6be479fd47
Erastus Murungi <erastuswagwan@gmail.com>
2025-02-15 14:16:05 -0800

    improve formatting

--- src/app/sudoku/sudoku-stats.tsx
+++ src/app/sudoku/sudoku-stats.tsx
@@ -13,8 +13,8 @@ const SudokuStat: React.FC<{ label: string; value: number | string }> = ({
     value,
 }) => (
     <div className="mr-8 flex flex-row items-center justify-between">
-        <span className="text-xs font-bold uppercase">{label}:&nbsp;</span>
-        <span className="text-xs uppercase">{value}</span>
+        <span className="text-xs font-bold">{label}:&nbsp;</span>
+        <span className="text-xs">{value}</span>
     </div>
 );
 
@@ -22,8 +22,8 @@ export const SudokuStats: React.FC<ReducerState> = (state: ReducerState) => {
     const { numMoves, numMistakes, score } = state;
     return (
         <div className="space-between bg-accent mb-2 flex h-8 w-full flex-row items-center justify-center rounded-sm border-4 p-1">
-            <SudokuStat label="Total Moves" value={numMoves} />
-            <SudokuStat label="Total Mistakes" value={numMistakes} />
+            <SudokuStat label="# Moves" value={numMoves} />
+            <SudokuStat label="# Mistakes" value={numMistakes} />
             <SudokuStat label="Score" value={score} />
         </div>
     );
